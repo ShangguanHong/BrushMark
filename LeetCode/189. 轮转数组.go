4ba2d7f25ce69530032ebd6e795186eb5ae4250b
ShangguanHong <sgh1450280694@gmail.com>
2022-05-07 14:58:22 +0800

    add problem

---
+++ LeetCode/189. 轮转数组.go
@@ -0,0 +1,16 @@
+package LeetCode
+
+func rotate(nums []int, k int) {
+	k %= len(nums)
+	reverse := func(nums []int) {
+		for i := 0; i < len(nums)/2; i++ {
+			nums[i], nums[len(nums)-i-1] = nums[len(nums)-i-1], nums[i]
+		}
+	}
+
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+
+	return
+}
---
+++ LeetCode/977. 有序数组的平方.go
@@ -0,0 +1,24 @@
+package LeetCode
+
+func sortedSquares(nums []int) []int {
+	var (
+		n           = len(nums) - 1
+		left, right = 0, n
+		res         = make([]int, n+1)
+	)
+
+	for left <= right {
+		num1 := nums[left] * nums[left]
+		num2 := nums[right] * nums[right]
+		if num1 > num2 {
+			res[n] = num1
+			left++
+		} else {
+			res[n] = num2
+			right--
+		}
+		n--
+	}
+
+	return res
+}
