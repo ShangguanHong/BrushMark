c56c9fe3a6cf699ae7ce8bc8b60b098e53c1912e
ShangguanHong <sgh1450280694@gmail.com>
2022-05-09 11:02:40 +0800

    add problem

---
+++ LeetCode/557. 反转字符串中的单词 III.go
@@ -0,0 +1,33 @@
+package LeetCode
+
+import (
+	"strings"
+)
+
+func reverseWords(s string) string {
+	var (
+		l, r = 0, 0
+		ss   strings.Builder
+	)
+
+	f := func() {
+		for i := r - 1; i >= l; i-- {
+			ss.WriteByte(s[i])
+		}
+	}
+
+	for r < len(s) {
+		if s[r] == ' ' {
+			f()
+			ss.WriteByte(' ')
+			l = r + 1
+		}
+		r++
+	}
+
+	if l != r {
+		f()
+	}
+
+	return ss.String()
+}
