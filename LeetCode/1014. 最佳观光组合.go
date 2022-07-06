f4d6fa151ee4d935bb39a7bbb476a9e1307df841
ShangguanHong <sgh1450280694@gmail.com>
2022-07-06 15:30:11 +0800

    add problem

---
+++ LeetCode/1014. 最佳观光组合.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 给你一个正整数数组 values，其中 values[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的 距离 为 j - i。
+//
+// 一对景点（i < j）组成的观光组合的得分为 values[i] + values[j] + i - j ，也就是景点的评分之和 减去 它们两者之间的距离。
+//
+// 返回一对观光景点能取得的最高分。
+//
+//
+//
+// 示例 1：
+//
+// 输入：values = [8,1,5,2,6]
+// 输出：11
+// 解释：i = 0, j = 2, values[i] + values[j] + i - j = 8 + 5 + 0 - 2 = 11
+// 示例 2：
+//
+// 输入：values = [1,2]
+// 输出：2
+//
+//
+// 提示：
+//
+// 2 <= values.length <= 5 * 104
+// 1 <= values[i] <= 1000
+//
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/best-sightseeing-pair
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func maxScoreSightseeingPair(values []int) int {
+	ans := values[0]
+	curMax := values[0]
+
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+
+		return y
+	}
+
+	for i := 1; i < len(values); i++ {
+		ans = max(ans, curMax+values[i]-i)
+		curMax = max(curMax, values[i]+i)
+	}
+
+	return ans
+}
+
+func main() {
+	fmt.Println(maxScoreSightseeingPair([]int{8, 1, 5, 2, 6}))
+	fmt.Println(maxScoreSightseeingPair([]int{1, 2}))
+}
